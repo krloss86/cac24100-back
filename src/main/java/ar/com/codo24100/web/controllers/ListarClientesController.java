3ff95caed19acbd437fd458034e1667c1c726101
carlos horacio lopez <carloshoracio.lopez@comafi.com.ar>
2024-06-24 21:28:52 -0300

    add api

--- src/main/java/ar/com/codo24100/web/controllers/ListarClientesController.java
+++ src/main/java/ar/com/codo24100/web/controllers/ListarClientesController.java
@@ -1,21 +1,37 @@
 package ar.com.codo24100.web.controllers;
 
+import java.io.IOException;
 import java.util.ArrayList;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import ar.com.codo24100.web.domain.Clientes;
 import ar.com.codo24100.web.service.ClientesService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-public class ListarClientesController {
+@WebServlet("/ListarClientesController")
+public class ListarClientesController extends HttpServlet{
 
-    public static void main(String[] args) {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //crear service
         ClientesService service = new ClientesService();
 
         //invocar metodo listarClientes > lista
         ArrayList<Clientes> clientes = service.listarClientes();
 
+        ObjectMapper mapper = new ObjectMapper();
+
+        //convierto Objecto java a json string
+		//ahora respondo al front: json, Convirtiendo el nuevo Clientes a json
+		String clientesJSON = mapper.writeValueAsString(clientes);
+        
         //mostrar por consola|enviar al frontend
-        System.out.println(clientes);
+        resp.getWriter().println(clientesJSON);
     }
 
 }
